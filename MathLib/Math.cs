2bf8396417e6c4bb98e386f0b0842994583fe4d9
Greg Doud <gpdoud@gmail.com>
2020-08-26 08:34:27 -0400

    "initial commit"

---
+++ MathLib/Math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MathLib {
+
+    public class Math {
+
+        public int Add(int a, int b) {
+            return a + b;
+        }
+        public int Subtract(int a, int b) {
+            return a - b;
+        }
+    }
+}
---
+++ TestMathLib/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace TestMathLib {
+    class Program {
+        static void Main(string[] args) {
+
+            var math = new MathLib.Math();
+            Console.WriteLine(math);
+            var ans = math.Add(1, 2);
+            ans = math.Subtract(5, 2);
+        }
+    }
+}
